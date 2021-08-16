55417d4dd992c5365d9a9e61adba924d3d7c336e
jhjeong <kkiu1756@gmail.com>
2021-08-17 00:27:52 +0900

    메서드 별 다른 Fetch 전략 적용

--- src/main/java/com/jhjeong/springdatajpapractice/post/CommentRepository.java
+++ src/main/java/com/jhjeong/springdatajpapractice/post/CommentRepository.java
@@ -1,7 +1,11 @@
 package com.jhjeong.springdatajpapractice.post;
 
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
+  @EntityGraph(attributePaths = "post", type = EntityGraphType.LOAD)
+  Comment getById(Long id);
 }
